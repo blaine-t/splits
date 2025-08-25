1f7eda5835bef953b3501adc753f488ce42986bd
blaine-t <blaine-t@bathost.net>
2025-08-24 23:11:06 -0500

    API Get/Post POC

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use axum::{Router};
+use axum::{Router, routing::get, routing::post};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
+        .route("/api", get(api_get))
+        .route("/api", post(api_post))
         .fallback_service(ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -11,4 +13,12 @@ async fn main() {
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn api_get() -> &'static str {
+    "Hello from GET!"
+}
+
+async fn api_post() -> &'static str {
+    "Hello from POST!"
 }
