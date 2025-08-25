ba1df1d3e75dbf392bd9bdde5afad3871f84a249
blaine-t <blaine-t@bathost.net>
2025-08-24 23:07:13 -0500

    Add support for static files on the root in Axum

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{Router};
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .fallback_service(ServeDir::new("static"));
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
 }
