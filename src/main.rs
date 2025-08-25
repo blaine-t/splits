4c124b618ae02d68f42a7483637a4ba9f5e208a1
blaine-t <blaine-t@bathost.net>
2025-08-24 23:58:33 -0500

    Update API to accept real data in POST requests and restructure API

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::{Router, routing::get, routing::post};
 use tower_http::services::ServeDir;
 use rusqlite::{Connection, Result};
+use axum::Json;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
@@ -19,11 +21,11 @@ async fn main() {
     ).unwrap();
 
     let app = Router::new()
-        .route("/api", get(api_get))
-        .route("/api", post(api_post))
+        .route("/api/v0/split/all", get(all_splits))
+        .route("/api/v0/split/new", post(new_split))
         .fallback_service(ServeDir::new("static"));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
@@ -44,7 +46,7 @@ fn get_connection() -> Result<Connection> {
     Connection::open("splits.db")
 }
 
-async fn api_get() -> String {
+async fn all_splits() -> String {
     let conn = get_connection().unwrap();
     let mut stmt = conn.prepare("SELECT id, user, is_down, is_elevator, duration_ms, timestamp FROM splits").unwrap();
     let splits = stmt.query_map([], |row| {
@@ -70,11 +72,19 @@ async fn api_get() -> String {
     result
 }
 
-async fn api_post() -> &'static str {
+#[derive(Deserialize)]
+struct SplitData {
+    user: String,
+    is_down: bool,
+    is_elevator: bool,
+    duration_ms: i32,
+}
+
+async fn new_split(Json(data): Json<SplitData>) -> &'static str {
     let conn = get_connection().unwrap();
     conn.execute(
         "INSERT INTO splits (user, is_down, is_elevator, duration_ms, timestamp) VALUES (?1, ?2, ?3, ?4, datetime('now'))",
-        ("test_user", false, true, 15500),
+        (&data.user, data.is_down, data.is_elevator, data.duration_ms),
     ).unwrap();
     "Data inserted!"
 }
